f5d46d2e10bbccf51cd9180de30f99321ae81fb4
DevImran1234 <devimran733@gmail.com>
2025-02-13 09:49:30 -0800

    dat

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
-	build: {
-		chunkSizeWarningLimit: 1000, // Increases the limit to 1000 kB
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
+  build: {
+    outDir: "dist", // Explicitly define the output directory
+    chunkSizeWarningLimit: 1000,
+  },
 });
